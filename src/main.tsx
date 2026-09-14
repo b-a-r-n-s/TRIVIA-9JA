6d9840e78b59a5f58ec3ab8bd234934e2f0a93f6
Adejo Barnabas <adejobarnabas0@gmail.com>
2026-09-14 13:51:50 +0100

    Wire final visual polish into app

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './styles.css'
 import './host.css'
 import './hints.css'
+import './final-ui.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
