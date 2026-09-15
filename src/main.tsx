5f1a80847252a2e257a5b0f8dc73e26a5a626b88
Adejo Barnabas <adejobarnabas0@gmail.com>
2026-09-15 10:58:10 +0100

    Use stable App with rebuilt mobile composition

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import './ui-overhaul.css'
-import App from './AppRebuild'
+import './legacy-home-fix.css'
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
