ac62635293755394eb1ab905619626cf2e16a2a2
Adejo Barnabas <adejobarnabas0@gmail.com>
2026-09-15 10:21:29 +0100

    Wire strict mobile viewport frame

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import './host.css'
 import './hints.css'
 import './final-ui.css'
 import './final-ui-polish.css'
+import './mobile-frame.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
