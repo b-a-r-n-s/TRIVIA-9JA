f94bd2b40d93c20df238069e59744d7ceb74b735
Adejo Barnabas <adejobarnabas0@gmail.com>
2026-09-15 10:52:17 +0100

    Rebuild UI foundation with one coherent stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
-import './host.css'
-import './hints.css'
-import './final-ui.css'
-import './final-ui-polish.css'
-import './mobile-frame.css'
-import './home-reference.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
