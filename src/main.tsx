37383043c5735e42f604a60f043f0b16de09ca00
Adejo Barnabas <adejobarnabas0@gmail.com>
2026-09-15 10:22:27 +0100

    Apply approved mobile home reference styles

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import './hints.css'
 import './final-ui.css'
 import './final-ui-polish.css'
 import './mobile-frame.css'
+import './home-reference.css'
 import App from './App'
 
 createRoot(document.getElementById('root')!).render(
