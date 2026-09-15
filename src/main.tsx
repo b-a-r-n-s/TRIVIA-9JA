615e586ba69ac31594bb9e7f6382e4280016590d
Adejo Barnabas <adejobarnabas0@gmail.com>
2026-09-15 11:16:12 +0100

    Use single final reference stylesheet

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
-import './ui-overhaul.css'
-import './legacy-home-fix.css'
+import './reference-final.css'
 import App from './AppRebuild'
 
 createRoot(document.getElementById('root')!).render(
