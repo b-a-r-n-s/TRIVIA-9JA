2bae3770543fabc62cf5d328552f25517b577be3
Adejo Barnabas <adejobarnabas0@gmail.com>
2026-09-15 10:54:33 +0100

    Switch runtime to the rebuilt application UI

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles.css'
 import './ui-overhaul.css'
-import App from './App'
+import App from './AppRebuild'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
